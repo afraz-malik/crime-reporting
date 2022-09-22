b0590e74135a7b2d9ff1a2e5f01bc23b43b607b1
Afraz Malik <afrazmalik321@gmail.com>
2022-09-23 00:38:33 +0500

    Updates

--- src/App.js
+++ src/App.js
@@ -25,7 +25,6 @@ const App = () => {
   }, [])
   return (
     <div className="App">
-      {/* <button onClick={() => dispatch(isUserAuthenticated())}>Hello</button> */}
       <ScrollTop />
       <Switch>
         <Route exact path="/" component={!fetching ? HomePage : Spinner} />
