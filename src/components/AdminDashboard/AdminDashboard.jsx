fa5add931b8a6df5a6f5433fa55c85bc481a7498
Afraz Malik <afrazmalik321@gmail.com>
2023-01-18 20:09:07 +0500

    Adding Changes

--- src/components/AdminDashboard/AdminDashboard.jsx
+++ src/components/AdminDashboard/AdminDashboard.jsx
@@ -5,80 +5,83 @@ import DashboardProfile from '../DashboardProfile/DashboardProfile'
 import DataBox from '../DataBox/DataBox'
 import FacultyRequests from '../FacultyRequests/FacultyRequests'
 import ReceivedMessages from '../ReceivedMessages/ReceivedMessages'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { getUsersStart } from '../../redux/user/user.action'
 import Form3 from '../AddReports/Form3'
 const AdminDashboard = ({ match }) => {
- const dispatch = useDispatch()
- React.useEffect(() => {
-  dispatch(getUsersStart())
- }, [dispatch])
- // dispatch(getUsersStart())
+  const dispatch = useDispatch()
+  React.useEffect(() => {
+    dispatch(getUsersStart())
+  }, [dispatch])
+  // dispatch(getUsersStart())
+  const currentUser = useSelector((state) => state.userReducer.currentUser)
+  console.log(currentUser)
+  let profile = false
+  let manage = false
+  let addReports = false
+  let messages = false
 
- let profile = false
- let manage = false
- let addReports = false
- let messages = false
-
- if (window.location.href.indexOf('manage-faculty') > -1) {
-  manage = true
- } else if (window.location.href.indexOf('add-report') > -1) {
-  addReports = true
- } else if (window.location.href.indexOf('messages') > -1) {
-  messages = true
- } else if (window.location.href.indexOf('dashboard') > -1) {
-  profile = true
- }
- return (
-  <div className={AdminDashboardCss.container}>
-   <nav>
-    <ul>
-     <li>
-      <Link
-       to={`${match.path}`}
-       style={
-        profile
-         ? {
-            color: '#d82a4e',
-            borderBottom: '5px solid #d82a4e',
-           }
-         : null
-       }
-      >
-       Profile
-      </Link>
-     </li>
-     <li>
-      <Link
-       to={`${match.path}/add-report`}
-       style={
-        addReports
-         ? {
-            color: '#d82a4e',
-            borderBottom: '5px solid #d82a4e',
-           }
-         : null
-       }
-      >
-       Report Crime
-      </Link>
-     </li>
-     <li>
-      <Link
-       to={`${match.path}/manage-faculty`}
-       style={
-        manage
-         ? {
-            color: '#d82a4e',
-            borderBottom: '5px solid #d82a4e',
-           }
-         : null
-       }
-      >
-       My Reports
-      </Link>
-     </li>
-     <li>
+  if (window.location.href.indexOf('manage-faculty') > -1) {
+    manage = true
+  } else if (window.location.href.indexOf('add-report') > -1) {
+    addReports = true
+  } else if (window.location.href.indexOf('messages') > -1) {
+    messages = true
+  } else if (window.location.href.indexOf('dashboard') > -1) {
+    profile = true
+  }
+  return (
+    <div className={AdminDashboardCss.container}>
+      <nav>
+        <ul>
+          <li>
+            <Link
+              to={`${match.path}`}
+              style={
+                profile
+                  ? {
+                      color: '#d82a4e',
+                      borderBottom: '5px solid #d82a4e',
+                    }
+                  : null
+              }
+            >
+              Profile
+            </Link>
+          </li>
+          {currentUser.type === 'reporter' && (
+            <li>
+              <Link
+                to={`${match.path}/add-report`}
+                style={
+                  addReports
+                    ? {
+                        color: '#d82a4e',
+                        borderBottom: '5px solid #d82a4e',
+                      }
+                    : null
+                }
+              >
+                Report Crime
+              </Link>
+            </li>
+          )}
+          <li>
+            <Link
+              to={`${match.path}/manage-faculty`}
+              style={
+                manage
+                  ? {
+                      color: '#d82a4e',
+                      borderBottom: '5px solid #d82a4e',
+                    }
+                  : null
+              }
+            >
+              Reported Crimes
+            </Link>
+          </li>
+          {/* <li>
       <Link
        to={`${match.path}/received-messages`}
        style={
@@ -92,19 +95,19 @@ const AdminDashboard = ({ match }) => {
       >
        Recived Messages
       </Link>
-     </li>
-    </ul>
-   </nav>
-   <Route exact path={`${match.path}`} component={DashboardProfile} />
-   <Route exact path={`${match.path}/manage-faculty`} component={DataBox} />
-   <Route exact path={`${match.path}/add-report`} component={Form3} />
-   <Route
-    exact
-    path={`${match.path}/received-messages`}
-    component={ReceivedMessages}
-   />
-  </div>
- )
+     </li> */}
+        </ul>
+      </nav>
+      <Route exact path={`${match.path}`} component={DashboardProfile} />
+      <Route exact path={`${match.path}/manage-faculty`} component={DataBox} />
+      <Route exact path={`${match.path}/add-report`} component={Form3} />
+      <Route
+        exact
+        path={`${match.path}/received-messages`}
+        component={ReceivedMessages}
+      />
+    </div>
+  )
 }
 
 export default withRouter(AdminDashboard)
--- src/components/DataBox/DataListGen.jsx
+++ src/components/DataBox/DataListGen.jsx
@@ -3,38 +3,59 @@ import EditBox from '../EditBox/EditBox'
 import DataBoxCss from './DataBox.module.scss'
 
 // Redux
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { deleteFaculty } from '../../redux/data/data.actions'
+import ReactModal from 'react-modal'
 // import { DeleteClientAction } from '../../redux/clients/clients.actions'
 // import { DeleteUserAction } from '../../redux/users/users.actions'
 // Components
 
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    height: '60vh',
+    width: '50vw',
+  },
+}
 const DataListGen = ({ data, index }) => {
- const dispatch = useDispatch()
- const [editBox, setEditBox] = useState(false)
- const toggleEditBox = () => {
-  setEditBox(!editBox)
- }
- const manageDelete = () => {
-  const result = window.confirm('Are you sure you want to delete?')
-  if (result) {
-   dispatch(deleteFaculty(data.id))
+  const dispatch = useDispatch()
+  const [openModal, setOpenModal] = useState(false)
+  const [editBox, setEditBox] = useState(false)
+  const currentUser = useSelector((state) => state.userReducer.currentUser)
+
+  const toggleEditBox = () => {
+    setEditBox(!editBox)
   }
- }
- console.log(data)
- return (
-  <tr>
-   <td>{data.type}</td>
-   <td>{data.date}</td>
-   <td>{data.time}</td>
-   <td>{data.details}</td>
-   <td>{data.location}</td>
-   <td>
-    <a href={data.imgurl} target='_href'>
-     <i className='fa fa-download'></i>
-    </a>
-   </td>
-   <td>
+  const manageDelete = () => {
+    const result = window.confirm('Are you sure you want to delete?')
+    if (result) {
+      dispatch(deleteFaculty(data.id))
+    }
+  }
+  console.log(data)
+  return (
+    <tr>
+      <td>{data.type}</td>
+      <td>{data.date}</td>
+      <td>{data.time}</td>
+      <td>{data.details}</td>
+      <td>{data.location}</td>
+      <td>
+        <a href={data.imgurl} target="_href">
+          <i className="fa fa-download"></i>
+        </a>
+      </td>
+      <td>
+        {currentUser.type === 'officer' && (
+          <i className="fa fa-eye" onClick={() => setOpenModal(true)}></i>
+        )}
+      </td>
+      {/* <td>
     <div className={DataBoxCss.actions}>
      <img alt='' src='images/edit.svg' onClick={() => toggleEditBox()} />
      <img alt='' src='images/delete.svg' onClick={() => manageDelete()} />
@@ -42,9 +63,30 @@ const DataListGen = ({ data, index }) => {
       <EditBox toggleEditBox={toggleEditBox} data={data} index={index} />
      ) : null}
     </div>
-   </td>
-  </tr>
- )
+   </td> */}
+      <ReactModal
+        isOpen={openModal}
+        onRequestClose={() => setOpenModal(false)}
+        style={customStyles}
+      >
+        <h1>Reporter Details</h1>
+        <table className={DataBoxCss.table}>
+          <tr>
+            <th>Reporter Name</th>
+            <th>Reporter Address</th>
+            <th>Reporter Phone</th>
+            <th>Reporter email</th>
+          </tr>
+          <tr>
+            <td>{data.displayName}</td>
+            <td>{data.address}</td>
+            <td>{data.phone}</td>
+            <td>{data.email}</td>
+          </tr>
+        </table>
+      </ReactModal>
+    </tr>
+  )
 }
 
 export default DataListGen
